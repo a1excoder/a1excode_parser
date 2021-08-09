ce48afccb2be4fb440717c0a96d936d0d06e9367
Aleksandr Rudenko <49398367+a1excoder@users.noreply.github.com>
2021-08-10 01:05:05 +0300

    upload not works code :(

--- main.py
+++ main.py
@@ -2,19 +2,47 @@ import requests as req
 from bs4 import BeautifulSoup
 
 
-class Parser:
+class Parser(object):
     def __init__(self):
         self.url = "https://a1excode.netxisp.host/"
-        self.soup = BeautifulSoup(self.get_page_query(), 'html.parser')
+        self.soup = BeautifulSoup(self.__get_page_query(), 'html.parser')
 
-    def get_page_query(self):
+    def __get_page_query(self):
         return req.get(self.url).text
 
-    def parse_six_last_news(self):
-        pass
+    def __parse_six_last_native(self):
+        return self.soup.find_all('div', class_="col-md-6")
+
+    def view_six_last_news_native(self):
+        n = 3
+        while n < 21:
+            if n % 3:
+                print(self.__parse_six_last_native()[n], "\n\n")
+                n = n + 1
+            n = n + 1
+
+    def get_list_six_last_news(self):
+        news_list = []
+        true_list = []
+
+        n = 3
+        while n < 21:
+            if n % 3:
+                news_list.append(self.__parse_six_last_native()[n])
+                n = n + 1
+            n = n + 1
+
+        for post in news_list:
+            true_list.append({
+                "title": BeautifulSoup(post, 'html.parser').find('h3', class_="font-weight-bold"),
+                "mini_text": BeautifulSoup(post, 'html.parser').find('p', class_="text-muted"),
+                "post_url": BeautifulSoup(post, 'html.parser').find('a', class_="btn btn-success")
+            })
+
+        return true_list
 
     def parse_top_post(self):
-        return self.soup.find('a')
+        return self.soup.find('div', {'class', 'six-last'})
 
     def get_title_page(self):
         return self.soup.title.string
@@ -22,4 +50,6 @@ class Parser:
 
 if __name__ == '__main__':
     parser = Parser()
-    print(parser.get_title_page())
+
+    for i in parser.get_list_six_last_news():
+        print(i)
